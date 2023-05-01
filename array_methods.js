fd00213cb495349d564323d67dead278ae218f2d
Akshay Narale <naraleakshay5@gmail.com>
2023-05-01 23:06:29 +0530

    find methods add

--- array_methods.js
+++ array_methods.js
@@ -3,3 +3,8 @@ console.log("From");
 const str = "hello";
 const arr = Array.from(str); // ['h', 'e', 'l', 'l', 'o']
 console.log(arr);
+
+// Find
+console.log("Find");
+const numbers = [1, 2, 3, 4, 5];
+const even = numbers.find((num) => num % 2 === 0); // 2
