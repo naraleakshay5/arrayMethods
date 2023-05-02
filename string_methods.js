57dec5cfcbc34e1ed3fe6078bcd2ec3414e0322f
Akshay Narale <naraleakshay5@gmail.com>
2023-05-02 09:32:29 +0530

    string methods add

--- string_methods.js
+++ string_methods.js
@@ -2,3 +2,8 @@
 console.log("toUpperCase");
 let name1 = "john doe";
 let upperCaseName = name1.toUpperCase(); // "JOHN DOE"
+
+//toLowerCase
+console.log("toLowerCase");
+let name2 = "JOHN DOE";
+let lowerCaseName = name2.toLowerCase(); // "john doe"
